58ee83a2e0857190dff0e434399349a287d41e29
Toru Choi <blueocean239@gmail.com>
2020-11-30 19:54:54 +0800

    uitextfield dismissing keyboard when touching outside

--- TSample/ViewController.swift
+++ TSample/ViewController.swift
@@ -7,9 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     let text:UILabel = UILabel()
+    let password:UITextField = UITextField()
     let button:UIButton = UIButton()
     
     override func viewDidLoad() {
@@ -18,13 +19,13 @@ class ViewController: UIViewController {
         initContainer()
         initCenterLabel()
         initBottomButton()
+        hideKeyboardWhenTappedAround()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         initTopBar()
         super.viewWillAppear(animated)
     }
-    
     
     func initTopBar() {
         self.navigationController?.setNavigationBarHidden(true, animated:false)
@@ -35,31 +36,32 @@ class ViewController: UIViewController {
         containerView.frame = UIScreen.main.bounds
         containerView.backgroundColor = .red
         self.view.addSubview(containerView)
-        
-//        containerView.translatesAutoresizingMaskIntoConstraints = false
-//        containerView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
-//        containerView.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
-        
     }
     
     func initCenterLabel() {
-        text.text = "Hello World"
-        text.textColor = .darkGray
-//        text.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
-        
-        text.textAlignment = NSTextAlignment.center
-        text.backgroundColor = .magenta
+        text.text = "TYPE YOUR PASSWORD."
+        text.textColor = .white
+        text.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        text.textAlignment = NSTextAlignment.left
         self.view.addSubview(text)
         
         text.translatesAutoresizingMaskIntoConstraints = false
-//        text.centerXAnchor.constraint(equalTo:self.view.centerXAnchor).isActive = true
-//        text.centerYAnchor.constraint(equalTo:self.view.centerYAnchor).isActive = true
+        text.centerYAnchor.constraint(equalTo:self.view.centerYAnchor).isActive = true
         
         let margins = view.layoutMarginsGuide
-        text.topAnchor.constraint(equalTo: margins.topAnchor).isActive = true
-        text.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0.0).isActive = true
-        text.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0.0).isActive = true
-        text.bottomAnchor.constraint(equalTo: margins.bottomAnchor, constant: 0.0).isActive = true
+        text.leadingAnchor.constraint(equalTo: margins.leadingAnchor, constant: 0.0).isActive = true
+        
+        password.font = UIFont.systemFont(ofSize: 20.0)
+        password.isSecureTextEntry = true // password
+        password.textContentType = UITextContentType.password
+        password.backgroundColor = .white
+        password.delegate = self
+        self.view.addSubview(password)
+        password.translatesAutoresizingMaskIntoConstraints = false
+        
+        password.topAnchor.constraint(equalTo: text.bottomAnchor, constant: 10.0).isActive = true
+        password.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
+        password.trailingAnchor.constraint(equalTo: margins.trailingAnchor).isActive = true
     }
     
     func initBottomButton() {
@@ -80,22 +82,28 @@ class ViewController: UIViewController {
     }
     
     @objc func btnClicked() {
-//        let newText = "Toru"
-//        if(text.text == "Hello World") {
-//            text.text = newText
-//        } else {
-//            text.text = "Hello World"
-//        }
         goToNextViewController()
     }
     
     private func goToNextViewController() {
         // present a modal with an embed UINavigationController
         let rvc = SecondViewController()
-//        let vc = UINavigationController(rootViewController: rvc)
-//        vc.modalPresentationStyle = .fullScreen
-//        present(vc, animated: true, completion: nil)
-        
         self.navigationController?.pushViewController(rvc, animated: true)
+    }
+    
+    func hideKeyboardWhenTappedAround() {
+        print("hideKeyboardWhenTappedAround")
+        let tap:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(endEditing))
+        tap.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tap)
+    }
+    
+    @objc func endEditing() {
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) ->Bool {
+        textField.resignFirstResponder()
+        return true
     }
 }
